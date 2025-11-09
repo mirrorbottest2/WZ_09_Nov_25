f4e25f316fe9ea9c1b573c2b8d5ffaf249b4ebf2
mirrorbottest2 <110727882+mirrorbottest2@users.noreply.github.com>
2025-11-09 12:02:05 +0530

    Update config_manager.py

--- bot/core/config_manager.py
+++ bot/core/config_manager.py
@@ -250,4 +250,4 @@ class BinConfig:
     QBIT_NAME = "stormtorrent"
     FFMPEG_NAME = "mediaforge"
     RCLONE_NAME = "ghostdrive"
-    SABNZBD_NAME = "newsripper"
+    #SABNZBD_NAME = "newsripper"
